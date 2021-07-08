6e2f7d45a56b093a32bcd15636a831353030e772
yash <yashponda5350@gmail.com>
2021-07-08 12:51:17 +0530

    src addedd

---
+++ React_Pract/ypapp/src/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata = [
+    {
+        imgsrc: "https://m.economictimes.com/thumb/msid-79640277,width-1200,height-900,resizemode-4,imgsize-520824/voot-select-thriller-asur-welcome-to-your-dark-side-featuring-barun-sobti-was-placed-at-sixth-position-.jpg",
+        title: "Top Web Series",
+        sname: "Scam 1992",
+        link: "https://www.sonyliv.com/shows/scam-1992-the-harshad-mehta-story-1700000292#:~:text=Set%20in%201980's%20and%2090's,behind%20India's%20biggest%20financial%20scam.",
+    },
+    {
+        imgsrc: "https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/The_Family_Man.jpeg/250px-The_Family_Man.jpeg",
+        title: "Top Web Series",
+        sname: "The Family man",
+        link: "https://www.primevideo.com/detail/0S3QYI59BAEI5KVLHCKSR91YGD/?ref_=dvm_pds_gen_in_as_s_g_dsafeedn",
+    },
+    {
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BNzRjMjhmZmUtYjRhNS00YjlmLWI4MzUtMDFhM2E0MjAyZGRjXkEyXkFqcGdeQXVyMTEwNDM0NTI2._V1_.jpg",
+        title: "Top Web Series",
+        sname: "Tandav",
+        link: "https://www.primevideo.com/detail/0TIXTH4G4FJBGABFDCLUEF1OU7/?ref_=dvm_pds_gen_in_as_s_g_dsafeedn",
+    },
+    {
+        imgsrc: "https://upload.wikimedia.org/wikipedia/en/3/39/Paatal_Lok_poster.jpg",
+        title: "Top Web Series",
+        sname: "Patal Lok",
+        link: "https://www.primevideo.com/detail/Paatal-Lok/0G9IEOHCN8KMY6COD9ILGH7IY5",
+    },
+    {
+        imgsrc: "https://upload.wikimedia.org/wikipedia/en/9/94/Sherni_film_poster.jpg",
+        title: "Top Web Series",
+        sname: "Sherni",
+        link: "https://www.primevideo.com/detail/Sherni/0S81WK2J8PA2G2F7JTBSVW3V6G",
+    },
+    {
+        imgsrc: "https://upload.wikimedia.org/wikipedia/en/thumb/f/fe/TVF_Aspirants_Poster.jpeg/250px-TVF_Aspirants_Poster.jpeg",
+        title: "Top Web Series",
+        sname: "Aspirants",
+        link: "https://youtu.be/ViOutJ0kuJY",
+    }
+];
+
+export default Sdata;
