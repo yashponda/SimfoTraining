ea1cfa3e2093b8fba74be43bdbe7d3141894bbde
yash <yashponda5350@gmail.com>
2021-07-13 13:03:49 +0530

    Calculation working

--- JS_Pract/JS_Calculator/calc.js
+++ JS_Pract/JS_Calculator/calc.js
@@ -1,13 +1,40 @@
-// to display the value
+                                // to display the value by pressing key from calc
+
 function display(val){
     document.getElementById("root").value += val
-}
-/*
-function operation(){
-    let x = document.getElementById("root").value
-    let
+    let x = document.getElementById("root");
+    function ke(e){
+        console.log(e.type + '' + e.keyCode + '' + string.fromCharcode(e.keyCode));
+    }
+    x.addEventListener("keydown", ke, false);
+    x.addEventListener("keypress", ke, false);
+    x.addEventListener("keyup", ke, false);
 }
 
-function resat(){
-    document.getElementById("root").value = " "
-}*/
+                        //to perform the final operation after clicking =
+function oper(){   
+    let x = document.getElementById("root").value;
+    let y = eval(x);
+    document.getElementById("root").value = y
+    document.getElementById("root").addEventListener(e.keyCode(13), oper());
+    
+} 
+                    // to perform the final operation by clicking Enter key
+                    window.addEventListener("keydown", operation , false);
+                    function operation(key){
+                        if (key.keyCode == "13"){
+                            let x = document.getElementById("root").value;
+                            let y = eval(x);
+                            document.getElementById("root").value = y;
+                        }
+                    }
+                                            //Ascii key of esc =27
+                                            //Ascii key of enter =13
+
+            // To clear everything after clicking Escape key.               
+    window.addEventListener("keydown", resat, false);
+    function resat(key){
+        if (key.keyCode == "27"){
+            document.getElementById("root").value = " ";
+        }
+    }
